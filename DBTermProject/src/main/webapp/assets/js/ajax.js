e21bc3125c4381874c5189086581ebed423ce126
CHOINAGYUNG <39054758+NimUserG@users.noreply.github.com>
2022-06-23 15:41:57 +0900

    Delete DBTermProject directory

--- DBTermProject/src/main/webapp/assets/js/ajax.js
+++
@@ -1,9 +0,0 @@
-function acyncMovePage(url){
-    $.ajax({
-        url:url,
-        dtatType:"html",
-        type:"post",
-        success:function(result){
-            $("#main").html(result);
-        }
-})};
